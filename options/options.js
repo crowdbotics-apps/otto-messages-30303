d87d31ecf853e2444e4ad3659d2a1d4eef2efa92
Crowdbotics <team@crowdbotics.com>
2021-09-03 23:06:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "otto_messages_30303Identifier",
+  url: "https://otto_messages_30303Identifier.botics.co",
+  api: "https://otto_messages_30303Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
